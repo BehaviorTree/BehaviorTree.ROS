7dba541984f0f5c76650064d9a8cd5688a97686d
Davide Faconti <davide.faconti@eurecat.org>
2018-12-10 16:01:24 +0100

    simple logged added

---
+++ include/behaviortree_ros/loggers/rosout_logger.h
@@ -0,0 +1,38 @@
+#ifndef BT_ROSOUT_LOGGER_H
+#define BT_ROSOUT_LOGGER_H
+
+#include <behaviortree_cpp/loggers/abstract_logger.h>
+#include <ros/console.h>
+
+namespace BT
+{
+
+class RosoutLogger : public StatusChangeLogger
+{
+    static std::atomic<bool> ref_count;
+
+  public:
+    RosoutLogger(TreeNode* root_node,
+                 ros::console::Level verbosity_level = ros::console::Level::Info);
+
+    ros::console::Level getLevel() const;
+
+    // Accepts only Info and Debug
+    void setLevel(ros::console::Level level);
+
+    ~RosoutLogger() override;
+
+    virtual void callback(Duration timestamp,
+                          const TreeNode& node,
+                          NodeStatus prev_status,
+                          NodeStatus status) override;
+
+    virtual void flush() override;
+
+private:
+    ros::console::Level _level;
+};
+
+}   // end namespace
+
+#endif   //BT_ROSOUT_LOGGER_H
---
+++ src/loggers/rosout_logger.cpp
@@ -0,0 +1,69 @@
+#include "behaviortree_ros/loggers/rosout_logger.h"
+
+namespace BT
+{
+std::atomic<bool> RosoutLogger::ref_count(false);
+
+RosoutLogger::RosoutLogger(TreeNode* root_node, ros::console::Level verbosity_level) :
+    StatusChangeLogger(root_node),
+    _level(verbosity_level)
+{
+    bool expected = false;
+    if (!ref_count.compare_exchange_strong(expected, true))
+    {
+        throw std::logic_error("Only a single instance of RosoutLogger shall be created");
+    }
+}
+
+ros::console::Level RosoutLogger::getLevel() const
+{
+    return _level;
+}
+
+void RosoutLogger::setLevel(ros::console::Level level)
+{
+    if( level != ros::console::Level::Debug &&
+        level != ros::console::Level::Info )
+    {
+        throw std::invalid_argument("RosoutLogger::setLevel acepts only Debug or Info");
+    }
+    _level = level;
+}
+
+RosoutLogger::~RosoutLogger()
+{
+    ref_count.store(false);
+}
+
+void RosoutLogger::callback(Duration timestamp, const TreeNode& node, NodeStatus prev_status,
+                             NodeStatus status)
+{
+    constexpr const char* whitespaces = "                         ";
+    const size_t ws_count = strlen(whitespaces)-1;
+
+    const auto& node_name = node.name();
+
+    switch( _level )
+    {
+    case ros::console::Level::Debug :
+        ROS_DEBUG("[%s%s]: %s -> %s",  node_name.c_str(),
+                  &whitespaces[std::min(ws_count, node_name.size())],
+                  toStr(prev_status, true),
+                  toStr(status, true));
+        break;
+
+    case ros::console::Level::Info :
+        ROS_INFO("[%s%s]: %s -> %s",  node_name.c_str(),
+                  &whitespaces[std::min(ws_count, node_name.size())],
+                  toStr(prev_status, true),
+                  toStr(status, true));
+        break;
+    }
+}
+
+void RosoutLogger::flush()
+{
+
+}
+
+}   // end namespace
